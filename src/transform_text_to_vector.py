500ba6f2e0b10f77a81329d17922126269d61002
Martin <M.Dierikx@student.tudelft.nl>
2022-05-20 16:25:36 +0200

    Reformat the source code to adhere to the linters

--- src/transform_text_to_vector.py
+++ src/transform_text_to_vector.py
@@ -1,33 +1,60 @@
+# pylint: disable=W1401,C0103
+"""This module transforms text to vectors"""
+import numpy as np
 from scipy import sparse as sp_sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
-import numpy as np
 
 
 # Bag of words
-def bag_of_words(X_train, X_val, X_test, words_counts):
-    DICT_SIZE = 5000
-    INDEX_TO_WORDS = sorted(words_counts, key=words_counts.get, reverse=True)[:DICT_SIZE]####### YOUR CODE HERE #######
-    WORDS_TO_INDEX = {word:i for i, word in enumerate(INDEX_TO_WORDS)}
-    ALL_WORDS = WORDS_TO_INDEX.keys()
+def bag_of_words(x_train, x_val, x_test, words_counts):
+    """
+    x_train: the training data
+    x_val: the actual data
+    x_test: the testing data
+    words_counts: the number of words
 
-    X_train_mybag = sp_sparse.vstack([sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_train])
-    X_val_mybag = sp_sparse.vstack([sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_val])
-    X_test_mybag = sp_sparse.vstack([sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_test])
-    print('X_train shape ', X_train_mybag.shape)
-    print('X_val shape ', X_val_mybag.shape)
-    print('X_test shape ', X_test_mybag.shape)
+    returns the bags of data
+    """
+    DICT_SIZE = 5000
+    INDEX_TO_WORDS = sorted(words_counts, key=words_counts.get, reverse=True)[
+        :DICT_SIZE
+    ]  ####### YOUR CODE HERE #######
+    WORDS_TO_INDEX = {word: i for i, word in enumerate(INDEX_TO_WORDS)}
+    # ALL_WORDS = WORDS_TO_INDEX.keys()
+
+    x_train_mybag = sp_sparse.vstack(
+        [
+            sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE))
+            for text in x_train
+        ]
+    )
+    x_val_mybag = sp_sparse.vstack(
+        [
+            sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE))
+            for text in x_val
+        ]
+    )
+    x_test_mybag = sp_sparse.vstack(
+        [
+            sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE))
+            for text in x_test
+        ]
+    )
+    print("X_train shape ", x_train_mybag.shape)
+    print("X_val shape ", x_val_mybag.shape)
+    print("X_test shape ", x_test_mybag.shape)
 
     # row = X_train_mybag[10].toarray()[0]
     # non_zero_elements_count = (row>0).sum()
-    return X_train_mybag, X_val_mybag, X_test_mybag
+    return x_train_mybag, x_val_mybag, x_test_mybag
 
 
 def my_bag_of_words(text, words_to_index, dict_size):
     """
-        text: a string
-        dict_size: size of the dictionary
+    text: a string
+    dict_size: size of the dictionary
 
-        return a vector which is a bag-of-words representation of 'text'
+    return a vector which is a bag-of-words representation of 'text'
     """
     result_vector = np.zeros(dict_size)
 
@@ -38,38 +65,49 @@ def my_bag_of_words(text, words_to_index, dict_size):
 
 
 def test_my_bag_of_words():
-    words_to_index = {'hi': 0, 'you': 1, 'me': 2, 'are': 3}
-    examples = ['hi how are you']
+    """
+    tests the bag of words
+    """
+    words_to_index = {"hi": 0, "you": 1, "me": 2, "are": 3}
+    examples = ["hi how are you"]
     answers = [[1, 1, 0, 1]]
     for ex, ans in zip(examples, answers):
         if (my_bag_of_words(ex, words_to_index, 4) != ans).any():
-            return "Wrong answer for the case: '%s'" % ex
-    return 'Basic tests are passed.'
+            return f"Wrong answer for the case: {ex}"
+    return "Basic tests are passed."
 
 
 # TFIDF
-def initialize_tfidf_vectorizer(X_train, X_val, X_test):
-    X_train_tfidf, X_val_tfidf, X_test_tfidf, tfidf_vocab = tfidf_features(X_train, X_val, X_test)
-    tfidf_reversed_vocab = {i:word for word,i in tfidf_vocab.items()}
-    return X_train_tfidf, X_val_tfidf, X_test_tfidf, tfidf_vocab, tfidf_reversed_vocab
-
-
-def tfidf_features(X_train, X_val, X_test):
+def initialize_tfidf_vectorizer(x_train, x_val, x_test):
     """
-        X_train, X_val, X_test — samples
-        return TF-IDF vectorized representation of each sample and vocabulary
+    x_train: the training data
+    x_val: the actual data
+    x_test: the testing data
+
+    returns the data for tfidf
+    """
+    x_train_tfidf, x_val_tfidf, x_test_tfidf, tfidf_vocab = tfidf_features(
+        x_train, x_val, x_test
+    )
+    tfidf_reversed_vocab = {i: word for word, i in tfidf_vocab.items()}
+    return x_train_tfidf, x_val_tfidf, x_test_tfidf, tfidf_vocab, tfidf_reversed_vocab
+
+
+def tfidf_features(x_train, x_val, x_test):
+    """
+    x_train, x_val, x_test — samples
+    return TF-IDF vectorized representation of each sample and vocabulary
     """
     # Create TF-IDF vectorizer with a proper parameters choice
     # Fit the vectorizer on the train set
     # Transform the train, test, and val sets and return the result
 
+    tfidf_vectorizer = TfidfVectorizer(
+        min_df=5, max_df=0.9, ngram_range=(1, 2), token_pattern="(\S+)"
+    )  ####### YOUR CODE HERE #######
 
-    tfidf_vectorizer = TfidfVectorizer(min_df=5, max_df=0.9, ngram_range=(1,2), token_pattern='(\S+)') ####### YOUR CODE HERE #######
+    x_train = tfidf_vectorizer.fit_transform(x_train)
+    x_val = tfidf_vectorizer.transform(x_val)
+    x_test = tfidf_vectorizer.transform(x_test)
 
-    X_train = tfidf_vectorizer.fit_transform(X_train)
-    X_val = tfidf_vectorizer.transform(X_val)
-    X_test = tfidf_vectorizer.transform(X_test)
-
-    return X_train, X_val, X_test, tfidf_vectorizer.vocabulary_
-
-
+    return x_train, x_val, x_test, tfidf_vectorizer.vocabulary_
