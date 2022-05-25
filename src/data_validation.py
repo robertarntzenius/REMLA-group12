a918092ed7cd31db60727722d61fea4f71a988f8
Martin <M.Dierikx@student.tudelft.nl>
2022-05-25 20:00:12 +0200

    Update the data validation to create a report of the data

--- src/data_validation.py
+++ src/data_validation.py
@@ -4,19 +4,29 @@ import tensorflow_data_validation as tfdv
 
 from src import preprocessing
 
-train = preprocessing.read_data("../data/train.tsv")
-validation = preprocessing.read_data("../data/validation.tsv")
-# test = pd.read_csv("data/test.tsv", sep="\t")
 
-train_stats = tfdv.generate_statistics_from_dataframe(train)
-validation_stats = tfdv.generate_statistics_from_dataframe(validation)
-tfdv.visualize_statistics(
-    rhs_statistics=train_stats,
-    lhs_statistics=validation_stats,
-    rhs_name="TRAIN",
-    lhs_name="VALIDATE",
-)
-train_schema = tfdv.infer_schema(train_stats)
-tfdv.display_schema(train_schema)
-anomalies = tfdv.validate_statistics(validation_stats, schema=train_schema)
-tfdv.display_anomalies(anomalies)
+def main():
+    train = preprocessing.read_data("../data/train.tsv")
+    validation = preprocessing.read_data("../data/validation.tsv")
+    # test = pd.read_csv("data/test.tsv", sep="\t")
+    print(train.shape)
+    train_stats = tfdv.generate_statistics_from_csv(data_location="../data/train.tsv", delimiter="\t")
+    validation_stats = tfdv.generate_statistics_from_csv(data_location="../data/validation.tsv", delimiter="\t")
+    stats = tfdv.get_statistics_html(
+        rhs_statistics=train_stats,
+        lhs_statistics=validation_stats,
+        rhs_name="TRAIN",
+        lhs_name="VALIDATE",
+    )
+    with open("../data/stats.html", "w") as f:
+        f.write(stats)
+    train_schema = tfdv.infer_schema(train_stats)
+    tfdv.display_schema(train_schema)
+    anomalies = tfdv.validate_statistics(validation_stats, schema=train_schema)
+    tfdv.display_anomalies(anomalies)
+    tfdv.write_stats_text(train_stats, "../data/stats.txt")
+    tfdv.write_anomalies_text(anomalies, "../data/anomalies.txt")
+
+
+if __name__ == "__main__":
+    main()
