f905e3cfdeb98c036a038079546e94e55a0b035e
robertarntzenius <robertarntzenius@github.com>
2022-06-10 15:00:56 +0200

    removed comments

--- flaskapi.py
+++ flaskapi.py
@@ -15,68 +15,9 @@ import main as predictor
 app = Flask(__name__)
 swagger = Swagger(app)
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     """
-#     Predict whether an SMS is Spam.
-#     ---
-#     consumes:
-#       - application/json
-#     parameters:
-#         - name: input_data
-#           in: body
-#           description: message to be classified.
-#           required: True
-#           schema:
-#             type: object
-#             required: sms
-#             properties:
-#                 sms:
-#                     type: string
-#                     example: This is an example of an SMS.
-#     responses:
-#       200:
-#         description: "The result of the classification: 'spam' or 'ham'."
-#     """
-#     input_data = request.get_json()
-#     sms = input_data.get('sms')
-#     processed_sms = prepare(sms)
-#     model = joblib.load('output/model.joblib')
-#     prediction = model.predict(processed_sms)[0]
-#
-#     res = {
-#         "result": prediction,
-#         "classifier": "decision tree",
-#         "sms": sms
-#     }
-#     print(res)
-#     return jsonify(res)
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
-#     """
-#     Predict whether a given SMS is Spam or Ham (dumb model: always predicts 'ham').
-#     ---
-#     consumes:
-#       - application/json
-#     parameters:
-#         - name: input_data
-#           in: body
-#           description: message to be classified.
-#           required: True
-#           schema:
-#             type: object
-#             required: sms
-#             properties:
-#                 sms:
-#                     type: string
-#                     example: This is an example of an SMS.
-#     responses:
-#       200:
-#         description: "The result of the classification: 'spam' or 'ham'."
-#     """
-    # input_data = request.get_json()
-    # sms = input_data.get('sms')
     predictor.predict()
     with open("reports/bag-of-words-metrics.json", "r") as file:
         prediction = json.load(file)
